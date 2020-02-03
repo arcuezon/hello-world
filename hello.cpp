9cf4b3c2ac9667b13a00bc1ffa72b6ade48d59a0
arcuezon <alfonsocuezon@gmail.com>
2020-02-03 19:34:54 +0800

    Added Age

--- hello.cpp
+++ hello.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main()
 {
     string name;
-
+    int age = 0;
 
     cout << "What is your name?";
     cin >> name;
+    cout << "How old are you?";
+    cin >> age;
 
-    cout << "Hello " << name << "!";
+    cout << "Hello " << name << "! You are " << age << " years old.";
 
     return 0;
 }
